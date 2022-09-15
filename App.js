d7961d1b3e1991592f8cd34541ceae9d0d57e39c
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-09-15 17:13:06 +0530

    Final touches

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { Button, FlatList, ScrollView, StyleSheet, View } from "react-native";
 import GoalInput from "./components/GoalInput";
@@ -30,44 +31,47 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title="Add New Goal"
-        color="#5e0acc"
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput
-        visible={isModalVisible}
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-      />
-      <View style={styles.goalsContainer}>
-        {/* scrollview renders all its content at a time, so FlatList is preferred for lazy loading of list-items */}
-        {/* <ScrollView>
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color="#a065ec"
+          onPress={startAddGoalHandler}
+        />
+        <GoalInput
+          visible={isModalVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          {/* scrollview renders all its content at a time, so FlatList is preferred for lazy loading of list-items */}
+          {/* <ScrollView>
         {courseGoals.map((goal) => (
           <View style={styles.goalItem} key={goal}>
             <Text style={styles.goalText}>{goal}</Text>
           </View>
         ))}
         </ScrollView> */}
-        {/* don't need a key extractor if the Item contains a 'key' property, so this property will be selected as key automatically */}
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalHandler}
-              />
-            );
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-        />
+          {/* don't need a key extractor if the Item contains a 'key' property, so this property will be selected as key automatically */}
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
